e8be6dc022d8b08517ba53755f72a6cbfdb17441
Xu Zhong <xu@quickly.co>
2017-02-22 23:10:54 -0800

    IOS-149 separate collection view and table view

--- API-SampleApp/API-SampleApp/CollectionViewController.swift
+++ API-SampleApp/API-SampleApp/CollectionViewController.swift
@@ -7,22 +7,23 @@
 //
 
 import UIKit
+import SafariServices
 
 class CollectionViewController: UIViewController {
-    
+    var model: SuggestionData?
 }
 
 extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         guard let model = model else { return 0 }
-        guard let cards = model[collectionView.tag].cards else { return 0 }
+        guard let cards = model.cards else { return 0 }
         return cards.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
         guard let model = model else { return cell }
-        guard let cards = model[collectionView.tag].cards else { return cell }
+        guard let cards = model.cards else { return cell }
         let cardData = cards[indexPath.row]
         cell.url = cardData.url
         return cell
@@ -33,7 +34,7 @@ extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDa
         cell.load()
         
         guard let model = model else { return }
-        guard let cards = model[collectionView.tag].cards else { return }
+        guard let cards = model.cards else { return }
         let cardData = cards[indexPath.row]
         if cardData.impressionurl != nil {
             cardData.trackImpression()
@@ -42,7 +43,7 @@ extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDa
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let model = model else { return }
-        guard let cards = model[collectionView.tag].cards else { return }
+        guard let cards = model.cards else { return }
         let cardData = cards[indexPath.row]
         guard let url = cardData.actionurl else { return }
         if #available(iOS 9.0, *) {
--- API-SampleApp/API-SampleApp/RootViewController.swift
+++ API-SampleApp/API-SampleApp/RootViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SafariServices
 import CoreLocation
 
 class RootViewController: UIViewController {
@@ -58,7 +57,9 @@ extension RootViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CarouselCell", for: indexPath) as! TableViewCell
         guard let model = model else { return cell }
         cell.suggestionLabel.text = model[indexPath.row].suggestion
-        cell.setCollectionViewDataSourceDelegate(self, forRow: indexPath.row)
+        let collectionVC = CollectionViewController()
+        collectionVC.model = model[indexPath.row]
+        cell.setCollectionViewDataSourceDelegate(collectionVC, forRow: indexPath.row)
         return cell
     }
 }
@@ -66,51 +67,6 @@ extension RootViewController: UITableViewDataSource {
 extension RootViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return CGFloat(80)
-    }
-}
-
-extension RootViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        guard let model = model else { return 0 }
-        guard let cards = model[collectionView.tag].cards else { return 0 }
-        return cards.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
-        guard let model = model else { return cell }
-        guard let cards = model[collectionView.tag].cards else { return cell }
-        let cardData = cards[indexPath.row]
-        cell.url = cardData.url
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        let cell = cell as! CollectionViewCell
-        cell.load()
-        
-        guard let model = model else { return }
-        guard let cards = model[collectionView.tag].cards else { return }
-        let cardData = cards[indexPath.row]
-        if cardData.impressionurl != nil {
-            cardData.trackImpression()
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let model = model else { return }
-        guard let cards = model[collectionView.tag].cards else { return }
-        let cardData = cards[indexPath.row]
-        guard let url = cardData.actionurl else { return }
-        if #available(iOS 9.0, *) {
-            let vc = SFSafariViewController(url: url)
-            self.present(vc, animated: true, completion: nil)
-        } else {
-            // Fallback on earlier versions
-        }
-        if cardData.clickurl != nil {
-            cardData.trackClick()
-        }
     }
 }
 
